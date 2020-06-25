a6f9767ebbb41d192a6c53161eaa3c2e60b1e108
shunp <shunpei42ba@gmail.com>
2020-06-25 16:53:21 +0900

    remove unused files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,18 @@
+const path = require('path')
+
 module.exports = {
   siteMetadata: {
     title: 'gatsby-three-ts-plus',
-    description: 'A starter kit for TypeScript-based Gatsby projects with sensible defaults.',
-    keywords: 'gatsbyjs, gatsby, javascript, sample, something',
+    description: 'A starter kit for Three.js scenes using a TypeScript-based Gatsby framework with emotion and tailwindcss.',
+    keywords: 'three.js, typescript, gatsbyjs, gatsby, emotion, tailwindcss',
     siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
     author: {
-      name: 'Resi Respati',
-      url: 'https://twitter.com/resir014',
+      name: 'Shumpei Koike',
+      url: 'https://twitter.com/shunpei42ba_',
       email: 'resir014@gmail.com'
     }
   },
   plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'content',
-        path: `${__dirname}/src/content`
-      }
-    },
     {
       resolve: 'gatsby-transformer-remark',
       options: {
@@ -54,6 +49,8 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-robots-txt'
   ]
 }
