1cdd14e75592138d3f51f5309d33271bce0204ae
shunp <shunpei42ba@gmail.com>
2020-06-28 15:45:18 +0900

    add base page

---
+++ src/pages/base.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import IndexLayout from '../layouts'
+import BaseScene from '../scenes/BaseScene'
+
+const PageTwo = () => (
+  <IndexLayout>
+    <BaseScene />
+  </IndexLayout>
+)
+
+export default PageTwo
